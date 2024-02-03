fe0fe089a36013d72fdc5aac694f249637962a8f
InajeGarcia <155758080+InajeGarcia@users.noreply.github.com>
2024-02-04 01:26:52 +0800

    check

--- lib/colleges/carch.dart
+++ lib/colleges/carch.dart
@@ -5,7 +5,10 @@ class CarchCollege extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CollegeDetailsScreen(
-      College('College of Architecture', 'images/carch.jpg'),
+      College(
+        'College of Architecture', 'images/carch.jpg',
+        //'''The College of Criminal Justice Education focuses on training future law enforcement professionals. Students learn about criminal law, criminology, and the principles of justice.'''
+      ),
     );
   }
 }

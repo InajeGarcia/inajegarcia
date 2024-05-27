84f8965a5f3186808cadf41a236503aa9472e318
InajeGarcia <155758080+InajeGarcia@users.noreply.github.com>
2024-05-28 01:21:44 +0800

    fgf

---
+++ lib/others/sfa.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SFa extends StatelessWidget {
+  final List<Map<String, String>> firstFloorImages = [
+    {'imagePath': 'images/spot/scbuilding.webp', 'label': 'SFA'},
+
+    // Add more image URLs as needed for the first floor
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Available Rooms'),
+      ),
+      body: ListView.builder(
+        itemCount: firstFloorImages.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: Image.asset(
+                    firstFloorImages[index]['imagePath']!,
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 8,
+                  left: 8,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      firstFloorImages[index]['label']!,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
